6aa24cc07ea349a2327cba183aea99f20fe51f00
Anwesh43 <anweshthecool0@gmail.com>
2020-11-24 04:03:20 +0530

    adding CircleStyleDiv

---
+++ src/CircleLineDiv.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useStyle} from './hooks'
+
+const CircleLineDiv = ({scale, w, h, onClick}) => {
+  const {circleStyle, lineStyle} = useStyle(w, h, scale)
+  return (
+    <div onClick = {onClick}>
+      <div style = {circleStyle()}></div>
+      <div style = {lineStyle()}></div>
+    </div>
+  )
+}
+
+export default CircleLineDiv
